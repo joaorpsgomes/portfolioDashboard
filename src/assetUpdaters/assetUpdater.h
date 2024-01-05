f38e0c780d73bdfe7d1f16bd63ed7da5080fde83
joaorpsgomes <joaorpsgomes@gmail.com>
2024-01-05 17:21:12 +0000

    Create base for asset updaters
    Issue: #9 #10 #11

---
+++ src/assetUpdaters/assetUpdater.h
@@ -0,0 +1,10 @@
+//
+// Created by joaorpsgomes on 1/5/24.
+//
+
+#ifndef ASSETUPDATER_H
+#define ASSETUPDATER_H
+
+class assetUpdater {};
+
+#endif // ASSETUPDATER_H
---
+++ src/assetUpdaters/caUpdater/caUpdater.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joaorpsgomes on 1/5/24.
+//
+
+#include "caUpdater.h"
